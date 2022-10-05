2f251ae214564d110361662c88b1a851f5263df4
TristynKahrs <79658190+TristynKahrs@users.noreply.github.com>
2022-10-05 09:53:37 -0600

    Config now always fills with defaults on startup
    A user can leave a field empty and it will now fill to default; showing them what settings their trades are under and providing an option to play with settings

--- config.py
+++ config.py
@@ -1,27 +1,41 @@
 import json
 from pathlib import Path
 import os
+from collections import Counter
 
-config_file = 'config.json'
-default_config_file = 'default_config.json'
-
-
-def startup():
-    if os.path.isfile(config_file) and os.access(config_file, os.R_OK):
-        config = get_config()
+def startup(config_file: str):
+    if __exists__(config_file):
+        print("Using existing configuration")
+        config = get_config(config_file)
+        apply_default(config_file)
     else:
         with open(config_file, 'w') as file:
-            with open(default_config_file, 'r') as default_file:
+            with open('./data/default_config.json', 'r') as default_file:
                 default_config = json.load(default_file)
             json.dump(default_config, file, indent=4)
             config = default_config
-        print("Config created successfully")
+        print("Your configuration could not be found, one has been creaded for you")
+    return config
+
+def __exists__(config_file: str):
+    return os.path.isfile(config_file) and os.access(config_file, os.R_OK)
+
+def apply_default(config_file: str):
+    with open('./data/default_config.json', 'r') as default_file:
+        default_config = json.load(default_file)
+    config = get_config(config_file)
+    
+    for key, value in default_config.items():
+        if key not in config:
+            config[key] = value
+    with open(config_file, 'w') as file:
+        json.dump(default_config, file, indent=4)
     return config
         
-def get_config():
+def get_config(config_file: str):
     with open(config_file, 'r') as file:
         config = json.load(file)
     return config
 
 if __name__ == '__main__':
-    startup()
+    startup('config_file')
--- sparkplug.py
+++ sparkplug.py
@@ -1,20 +1,36 @@
-from config import startup
+from config import startup, apply_default
 
 def main():
-    #init config.py
-    #get config.json
-    config = startup()
+    #run cli
+    
+    config = startup('./data/config.json')
+    
+    login = config.get('startup').get('login')
+    if (login.get('username') == '') | (login.get('password') == ''):
+        print('Login information is required, please enter your brokerage account to try again.')
+        exit()
+    else:
+        #TODO check login info is valid
+        pass
+    
     print(config)
+    
+    apply_default('./data/config.json')
     #run universe selection
+    universe = config.get('universe')
+    
     #run algoritm selection
+    algorithm = config.get('algoritm')
+    
     #calculate risk
+    risk = config.get('risk')
+    
     #enter trades
         #display data
         #save data
         #generate reports
             #json->html->pdf
         #email reports
-    pass
 
 if __name__ == '__main__':
     main()
